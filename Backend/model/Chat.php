0a763fce48e687e9b2bf7c06c13d5c3408e11806
guediagael <e.guedia@innopolis.ru>
2016-12-14 18:25:38 +0300

    5  controllers (Chat,Event, Event organizator, Institution, Message) 5 models for from the controllers above.

--- Backend/model/Chat.php
+++ Backend/model/Chat.php
@@ -6,10 +6,45 @@
  * Date: 13/12/2016
  * Time: 17:27
  */
-class Chat {
 
-    private $chatId;
-    private $
+use Phalcon\Mvc\Model;
+
+class Chat extends Model{
+
+    private $chat_id;
+    private $chat_name;
+    /**
+     * @return mixed
+     */
+    public function getChatId()
+    {
+        return $this->chat_id;
+    }
+
+    /**
+     * @param mixed $chat_id
+     */
+    public function setChatId($chat_id)
+    {
+        $this->chat_id = $chat_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChatName()
+    {
+        return $this->chat_name;
+    }
+
+    /**
+     * @param mixed $chat_name
+     */
+    public function setChatName($chat_name)
+    {
+        $this->chat_name = $chat_name;
+    }
+
 
 
 }
